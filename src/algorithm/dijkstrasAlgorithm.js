9209d263e808e1658e03a26012817a934f82a990
Akshaykumar-2001 <100365654+Akshaykumar-2001@users.noreply.github.com>
2023-07-24 20:21:12 +0530

    Update dijkstrasAlgorithm.js

--- src/algorithm/dijkstrasAlgorithm.js
+++ src/algorithm/dijkstrasAlgorithm.js
@@ -5,10 +5,7 @@ export function dijkstrasAlgorithm(grid, startNode, finishNode) {
     while (!!unvisitedNodes.length) {
       sortNodesByDistance(unvisitedNodes);
       const closestNode = unvisitedNodes.shift();
-      // If we encounter a wall, we skip it.
       if (closestNode.isWall) continue;
-      // If the closest node is at a distance of infinity,
-      // we must be trapped and should therefore stop.
       if (closestNode.distance === Infinity) return visitedNodesInOrder;
       closestNode.isVisited = true;
       visitedNodesInOrder.push(closestNode);
@@ -51,9 +48,7 @@ export function dijkstrasAlgorithm(grid, startNode, finishNode) {
     
     return nodes;
   }
-  
-  // Backtracks from the finishNode to find the shortest path.
-  // Only works when called *after* the dijkstra method above.
+
   export function getNodesInShortestPathOrder(finishNode) {
     const nodesInShortestPathOrder = [];
     let currentNode = finishNode;
